524cf959bef713d316ccb1e1eb2163098b8f631b
Aki <akmalqa101@gmail.com>
2019-10-25 10:07:53 -0700

    add scroll button

--- script.js
+++ script.js
@@ -3,3 +3,19 @@ let navBarToggle = document.getElementById('js-navbar-toggle');
 navBarToggle.addEventListener('click', function () {
 mainNav.classList.toggle('active');
 });
+
+
+ //Get the button
+const mybutton = document.getElementById("myBtn");
+
+window.onscroll = () => scrollFunction();
+
+// When the user scrolls down 200px from the top of the document, show the button
+const scrollFunction = () => {
+  document.body.scrollTop > 200 || document.documentElement.scrollTop > 200 ?  mybutton.style.display = "block" : mybutton.style.display = "none";
+}
+// When the user clicks on the button, scroll to the top of the document
+const topFunction = () => {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
