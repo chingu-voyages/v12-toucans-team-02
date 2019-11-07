a02953ba6025c3384a0f9da253493c3498037218
Aki <akmalqa101@gmail.com>
2019-11-06 18:33:44 -0800

    add hover button to Solution nav

--- script.js
+++ script.js
@@ -5,6 +5,10 @@ mainNav.classList.toggle('active');
 });
 
 
+//make modal 
+
+ 
+
  //Get the button
 const mybutton = document.getElementById("myBtn");
 
@@ -20,3 +24,31 @@ const topFunction = () => {
   document.documentElement.scrollTop = 0;
 }
 
+
+
+// Get the modal
+let modal= document.getElementById("myModal");
+
+let btn = document.getElementById("myModalBtn");
+
+// Get the <span> element that closes the modal
+
+let span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onmouseover = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function(){
+  modal.style.display ="none";
+}
+// When the user clicks anywhere outside of the modal, close it
+
+window.onclick = function (event) {
+  if(event.target == modal){
+    modal.style.display = "none";
+  }
+}
+ 
